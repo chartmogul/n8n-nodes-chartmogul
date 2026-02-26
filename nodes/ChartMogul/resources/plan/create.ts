2f23c0a687f96cfe980bebe8254cc12896bfa0f9
Gef <gef@chartmogul.com>
2026-02-27 00:15:17 +0800

    file structure update - plan

---
+++ nodes/ChartMogul/resources/plan/create.ts
@@ -0,0 +1,66 @@
+import type { INodeProperties } from 'n8n-workflow';
+
+export const createDescription: INodeProperties[] = [
+    {
+        displayName: 'Data Source UUID',
+        name: 'data_source_uuid',
+        type: 'string',
+        default: '',
+        description: 'ChartMogul UUID of the Data Source the plan belongs to',
+        required: true,
+        displayOptions: { show: { resource: ['plan'], operation: ['create'] } },
+        routing: { request: { body: { data_source_uuid: '={{$value}}' } } },
+    },
+    {
+        displayName: 'Plan Name',
+        name: 'name',
+        type: 'string',
+        default: '',
+        required: true,
+        displayOptions: { show: { resource: ['plan'], operation: ['create'] } },
+        routing: { request: { body: { name: '={{$value}}' } } },
+    },
+    {
+        displayName: 'Plan Interval Count',
+        name: 'interval_count',
+        type: 'number',
+        typeOptions: { minValue: 1 },
+        default: 1,
+        description: 'The number of intervals between each billing cycle',
+        required: true,
+        displayOptions: { show: { resource: ['plan'], operation: ['create'] } },
+        routing: { request: { body: { interval_count: '={{$value}}' } } },
+    },
+    {
+        displayName: 'Plan Interval Unit',
+        name: 'interval_unit',
+        type: 'options',
+        default: 'month',
+        description: 'The unit of time for the plan interval',
+        options: [
+            { name: 'Day', value: 'day' },
+            { name: 'Month', value: 'month' },
+            { name: 'Year', value: 'year' },
+        ],
+        required: true,
+        displayOptions: { show: { resource: ['plan'], operation: ['create'] } },
+        routing: { request: { body: { interval_unit: '={{$value}}' } } },
+    },
+    {
+        displayName: 'Additional Fields',
+        name: 'additionalFields',
+        type: 'collection',
+        default: {},
+        placeholder: 'Define Plan External ID',
+        options: [
+            {
+                displayName: 'External ID',
+                name: 'externalId',
+                type: 'string',
+                default: '',
+                routing: { request: { body: { external_id: '={{$value}}' } } },
+            },
+        ],
+        displayOptions: { show: { resource: ['plan'], operation: ['create'] } },
+    },
+];
