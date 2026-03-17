2483f993e37f5588ca435459c9b8b8dec08ce7d4
Gef <gef@chartmogul.com>
2026-03-17 16:23:10 +0800

    refactor - subscription nodes (tested)

---
+++ nodes/ChartMogul/resources/subscription/index.ts
@@ -0,0 +1,67 @@
+import type { INodeProperties } from 'n8n-workflow';
+
+import { listDescription } from './list';
+import { connectDescription } from './connect';
+import { disconnectDescription } from './disconnect';
+
+export const subscriptionDescription: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		noDataExpression: true,
+		displayOptions: { show: { resource: ['subscription'] } },
+		options: [
+			{
+				name: "List Customer's Subscriptions",
+				value: 'list',
+				action: 'List subscriptions for a customer',
+				routing: {
+					request: { method: 'GET', url: '=/customers/{{$parameter.customerUUID}}/subscriptions' },
+					operations: {
+						pagination: {
+							type: 'generic',
+							properties: {
+								continue: '={{ $response.body.has_more === true }}',
+								request: { qs: { per_page: '={{$parameter.returnAll ? 200 : ($parameter.limit ?? 50)}}', cursor: '={{$response.body.cursor}}', } },
+							}
+						}
+					}
+				},
+			},
+			{
+				name: 'Connect Subscriptions',
+				value: 'connect',
+				action: 'Connect subscriptions of a customer',
+				routing: {
+					request: {
+						method: 'POST',
+						url: '=/customers/{{$parameter.customerUUID}}/connect_subscriptions',
+					},
+					// The API doesn't return anything, so we set a custom output to indicate success
+					output: {
+						postReceive: [{ type: 'set', properties: { value: '={{ { connected: true } }}' } }],
+					},
+				},
+			},
+			{
+				name: 'Disconnect Subscriptions',
+				value: 'disconnect',
+				action: 'Disconnect subscriptions of a customer',
+				routing: {
+					request: {
+						method: 'POST',
+						url: '=/customers/{{$parameter.customerUUID}}/disconnect_subscriptions',
+					},
+					output: {
+						postReceive: [{ type: 'set', properties: { value: '={{ { disconnected: true } }}' } }],
+					},
+				},
+			},
+		],
+		default: 'list',
+	},
+	...listDescription,
+	...connectDescription,
+	...disconnectDescription,
+];
